660730503d0169fb5ae2d97191ad305cc7c315cd
Mark Feineigle <mfeineigle@gmail.com>
2023-10-20 11:00:12 -0400

    fix bullet look_at() direction. Rename bullet sprite2Ds.
    Godot 4.1.2 broke bullet look_at(). Fixed.
    
    Sprite2Ds renamed to RifleBullet and ShotgunBullet
    to match RocketBullet of the rocket launcher.

--- projectiles/bullet.gd
+++ projectiles/bullet.gd
@@ -6,20 +6,17 @@ extends Area2D
 var direction: Vector2
 
 func setup(dir) -> void:
-	# TODO cleaner offsetting
-#	var offset = Globals.player_pos + (dir.normalized() * 100)
-
-#	position = offset
 	position = Globals.player_pos
 	if not dir:
 		direction = Vector2.RIGHT
 	else:
 		direction = dir
-	look_at(dir)
+	look_at(self.global_position + (direction))
 
 
 func _process(_delta) -> void:
 	position += direction.normalized() * speed
+	
 	
 	
 func _on_body_entered(body) -> void:
@@ -29,8 +26,3 @@ func _on_body_entered(body) -> void:
 				damage -= 1
 			body.hit(damage)
 		queue_free()
-
-
-#func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	#print("bullet entered area ", area)
-	#queue_free()
