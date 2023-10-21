650c020e16d712efde75365af540b513f3b67048
Mark Feineigle <mfeineigle@gmail.com>
2023-10-21 13:14:59 -0400

    fix unused param warning: delta to _delta

--- enemies/tank.gd
+++ enemies/tank.gd
@@ -3,7 +3,7 @@ extends GroundVehicle
 func _ready():
 	direction = Vector2.RIGHT
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	velocity = direction * speed
 	Utils.flip_h_sprite_direction(vehicle_sprite, direction)
 	move_and_slide()
