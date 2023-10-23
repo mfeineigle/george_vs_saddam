187ed1074844979a1ad4457836d8299d37388340
Mark Feineigle <mfeineigle@gmail.com>
2023-10-23 08:00:39 -0400

    add return value rotate_sprite_direction() -> void

--- globals/utils.gd
+++ globals/utils.gd
@@ -13,7 +13,7 @@ func flip_v_sprite_direction(sprite: Sprite2D, dir: Vector2) -> void:
 	elif dir.x < -0.1:
 		sprite.flip_v = true
 
-func rotate_sprite_direction(sprite: Sprite2D, rot):
+func rotate_sprite_direction(sprite: Sprite2D, rot) -> void:
 	if rot < PI/2 and rot >= -PI/2:
 		sprite.flip_v = false
 	else:
