1063bc876dd758cb840bd2641482567a1cddc0a3
Mark Feineigle <mfeineigle@gmail.com>
2023-10-26 17:04:19 -0400

    update look_away()

--- allies/civilian.gd
+++ allies/civilian.gd
@@ -15,27 +15,13 @@ func _physics_process(_delta) -> void:
 	if rotation >= PI or rotation <= -PI:
 		rotation = 0
 	if run_away:
-		look_away()
+		look_away(Globals.player_pos)
 		direction = (Globals.player_pos - position).normalized()
 		velocity = speed * -direction
 		move_and_slide()
 	Utils.rotate_sprite_direction(sprite, rotation)
 
 
-func look_away() -> void:
-		var x = abs(Globals.player_pos.x - position.x)
-		if position.x > Globals.player_pos.x:
-			x = position.x + x
-		else:
-			x = position.x - x
-		var y = abs(Globals.player_pos.y - position.y)
-		if position.y > Globals.player_pos.y:
-			y = position.y + y
-		else:
-			y = position.y - y
-		look_at(Vector2(x, y))
-		
-		
 func _on_avoidance_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		run_away = true
@@ -47,7 +33,10 @@ func _on_avoidance_area_body_exited(body: Node2D) -> void:
 func _on_keep_running_timer_timeout() -> void:
 	run_away = false
 	
-	
+func look_away(target_pos):
+	rotate(get_angle_to(target_pos) + PI);
+
+
 func hit(dmg) -> void:
 	$HealthComponent.damage(dmg)
 	if $HealthComponent.destroyed:
@@ -55,4 +44,3 @@ func hit(dmg) -> void:
 
 func die() -> void:
 	queue_free()
-	
