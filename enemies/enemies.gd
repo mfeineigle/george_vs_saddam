31b56b1766c89d494f171f9369512dc793a0e7a4
Mark Feineigle <mfeineigle@gmail.com>
2023-11-08 22:35:34 -0500

    cleanup enemies.gd order   - aircraft/ground vehicles/soldiers     - packed scenes     - signal connects     - spawn functions

--- enemies/enemies.gd
+++ enemies/enemies.gd
@@ -1,31 +1,35 @@
 extends Node2D
 
-var guard_scene: PackedScene = preload("res://enemies/guard.tscn")
-var troop_scene: PackedScene = preload("res://enemies/troop.tscn")
+# Aircraft
+var an_26_scene: PackedScene = preload("res://enemies/an_26.tscn")
 var tu_22_scene: PackedScene = preload("res://enemies/tu_22.tscn")
 var can_tu_22: bool = true
+
+# Ground Vehicles
 var jeep_scene: PackedScene = preload("res://enemies/jeep.tscn")
+var soldier_transport_scene: PackedScene = preload("res://enemies/soldier_transport.tscn")
+var tank_scene: PackedScene = preload("res://enemies/tank.tscn")
+
+# Soldiers
+var guard_scene: PackedScene = preload("res://enemies/guard.tscn")
+var troop_scene: PackedScene = preload("res://enemies/troop.tscn")
 
 
 func _ready() -> void:
-	
+	# Aircraft
+	GameEvents.spawn_an_26.connect(_spawn_an_26)
+	GameEvents.spawn_tu_22.connect(_spawn_tu_22)
+	# Ground Vehicles
+	GameEvents.spawn_jeep.connect(_spawn_jeep)
+	GameEvents.spawn_soldier_transport.connect(_spawn_soldier_transport)
+	# Soldiers
 	GameEvents.spawn_guard.connect(_spawn_guard)
 	GameEvents.spawn_troop.connect(_spawn_troop)
-	GameEvents.spawn_tu_22.connect(_spawn_tu_22)
-	
-	GameEvents.spawn_jeep.connect(_spawn_jeep)
 
-
-func _spawn_guard(pos) -> void:
-	var guard = guard_scene.instantiate()
-	guard.setup(pos)
-	$Soldiers.add_child(guard)
-	
-	
-func _spawn_troop(pos) -> void:
-	var troop = troop_scene.instantiate()
-	troop.setup(pos)
-	$Soldiers.add_child(troop)
+func _spawn_an_26() -> void:
+	print("spawning an-26")
+	var an_26 = an_26_scene.instantiate()
+	$Aircraft.add_child(an_26)
 
 
 func _spawn_tu_22() -> void:
@@ -39,8 +43,27 @@ func _on_tu_22_timer_timeout() -> void:
 	can_tu_22 = true
 
 
+# ground vehicles
+func _spawn_soldier_transport() -> void:
+	pass
+
+
 func _spawn_jeep() -> void:
 	pass
+
+
+# soldiers
+func _spawn_guard(pos) -> void:
+	var guard = guard_scene.instantiate()
+	guard.setup(pos)
+	$Soldiers.add_child(guard)
+
+
+func _spawn_troop(pos) -> void:
+	var troop = troop_scene.instantiate()
+	troop.setup(pos)
+	$Soldiers.add_child(troop)
+
 
 #func _get_spawn_point():
 #	%PathFollow2D.progress_ratio = randf()
