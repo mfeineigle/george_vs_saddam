8c365e9acb7517424553b74c52f2b502b9508a13
Mark Feineigle <mfeineigle@gmail.com>
2023-11-13 22:11:45 -0500

    add fire column scene

---
+++ FireColumn.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var particles: GPUParticles2D = $GPUParticles2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+@export var delay: float = 1.0
+
+func _ready() -> void:
+	sprite.hide()
+	$Timer.wait_time = delay
+
+
+func _on_timer_timeout() -> void:
+	print("timer")
+	sprite.show()
+	sprite.play("burn")
+	particles.emitting = true
+	await sprite.animation_finished
+	#particles.emitting = false
+	sprite.hide()
