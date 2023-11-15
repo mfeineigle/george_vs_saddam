bfdf7316186a94269fe54f176763ca7b5d871860
Mark Feineigle <mfeineigle@gmail.com>
2023-11-15 00:48:41 -0500

    update radar tower
    - cleanup references to internal nodes
        - death_animation_player, health_component, rays
    - use area2D to limit raycast checking to when George is near
    - flash shader animation on hit
    - ping sound effect on detecting George

--- enemies/radar_tower.gd
+++ enemies/radar_tower.gd
@@ -1,33 +1,41 @@
 extends StaticBody2D
 
 @export var rotation_speed: float = 1.0
-
-
-func _ready() -> void:
-	var rays = get_node("Top").get_children()
-	for ray in rays:
-		print(ray)
+@onready var death_animation_player: AnimationPlayer = $DeathAnimationPlayer
+@onready var health_component: Node2D = $HealthComponent
+@onready var rays = get_node("Top").get_children()
+var player_near: bool = false
 
 
 func _process(delta):
 	check_radar(delta)
-	
-	
+
+
 func check_radar(delta) -> void:
-	if not $HealthComponent.destroyed:
+	if player_near and not health_component.destroyed:
 		$Top.rotation += rotation_speed * delta
-		for ray in get_node("Top").get_children(): # NOTE optimize with area2D and programatically
-			var collider = ray.get_collider()       # draw a few rays to test line of sight
+		for ray in rays:
+			var collider = ray.get_collider()
 			if collider in get_tree().get_nodes_in_group("player"):
-				print("radar collider: ", collider.name)
+				$AudioStreamPlayer2D.play()
 				GameEvents.spawn_tu_22.emit()
-			
+
 func _on_detection_area_body_entered(body):
-	print("radar detected ", body)
+	if body.is_in_group("player"):
+		player_near = true
 
 func _on_detection_area_body_exited(body):
-	print("radar out of range ", body)
-	
-	
+	if body.is_in_group("player"):
+		player_near = false
+
+
 func hit(dmg) -> void:
-	$HealthComponent.damage(dmg)
+	if not health_component.destroyed:
+		death_animation_player.play("hit")
+		health_component.damage(dmg)
+	if health_component.destroyed:
+		die()
+
+func die() -> void:
+	$DeathAnimation.show()
+	death_animation_player.play("die")
