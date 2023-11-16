fe2215b2e9c4a22bf29a448aed04e624d8cee255
Mark Feineigle <mfeineigle@gmail.com>
2023-11-16 08:23:20 -0500

    add landmine

---
+++ weapons/landmine.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+
+@export var damage: int = 5
+@export var knockback_strength: int = 200
+
+
+func _on_body_entered(body: Node2D) -> void:
+	var dir = global_position.direction_to(body.global_position)
+	if body.has_method("hit"):
+		body.hit(damage)
+	if body.has_method("knockback"):
+		body.knockback(dir, knockback_strength)
+	animation_player.play("explode")
+	audio_stream_player_2d.play()
+	await audio_stream_player_2d.finished
+	queue_free()
