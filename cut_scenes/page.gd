356da91e9c1b8137724ea354b45ec61de5d1d2bd
Mark Feineigle <mfeineigle@gmail.com>
2023-11-17 05:26:34 -0500

    add page.tscn/page.gd as base of cut scene pages
    - page.tscn will be a parent node all pages inherit from
      - individual page_xx.tscn have their own frames
    
    - page.gd:
      - skip the cut scene if click and hold
      - display next frame on click
      - load next_page after last frame
      - load next_level after last page/frame or skip

---
+++ cut_scenes/page.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@export var frames: Array[TextureRect]
+@export var next_page: PackedScene
+@export var next_level: PackedScene
+
+
+@onready var progress_bar: ProgressBar = $SkipScene/ProgressBar
+var value: float = 0
+
+var idx: int = 0
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		if idx < frames.size():
+			frames[idx].visible = true
+			idx += 1
+		else:
+			get_tree().change_scene_to_packed(next_page)
+
+
+func _on_skip_timer_timeout() -> void:
+	if Input.is_action_pressed("shoot"):  
+		progress_bar.value += 5  #Button is pressed, increase the progress
+		if progress_bar.value >= 10:
+			$SkipScene.visible = true
+	else:  
+		progress_bar.value = 0  #The button wasn't down during this tick, reset progress
+	if progress_bar.value >= 100:
+		get_tree().change_scene_to_packed(next_level)
