9b8f2a0ffcdda3ccdf0668a829b6c42a60ca4edb
Mark Feineigle <mfeineigle@gmail.com>
2023-11-25 11:12:47 -0500

    update to use global_position for follower

--- levels/path.gd
+++ levels/path.gd
@@ -2,8 +2,11 @@ extends PathFollow2D
 
 @export var speed: float
 @export var follower: GroundVehicle
+## progress_ratio start pickup position (eg: 0.3)
 @export var pick_up_lower: float
+## progress_ratio done pickup position (eg: 0.31)
 @export var pick_up_upper: float
+## how long to wait during pickup
 @export var wait_time: float = 1.0
 
 @onready var timer := Timer.new()
@@ -18,7 +21,7 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	follower.global_position = position
+	follower.global_position = global_position
 	if not (follower.get_node("HealthComponent").destroyed) and not pick_up:
 		if progress_ratio > pick_up_lower and progress_ratio < pick_up_upper:
 			timer.start()
@@ -31,3 +34,5 @@ func _process(delta: float) -> void:
 func _on_timer_timeout() -> void:
 	pick_up = false
 	progress_ratio = pick_up_upper
+	if follower.can_spawn_troops:
+		print("loading more soldiers")
