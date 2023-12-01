2ca3ca723bcfbf6dea51ad598ee1b71e53eb7dd1
Mark Feineigle <mfeineigle@gmail.com>
2023-12-01 11:41:10 -0500

    add shield hit sounds

--- enemies/guard.gd
+++ enemies/guard.gd
@@ -1,6 +1,9 @@
 extends Soldier
 
 @onready var shield_sprite: Sprite2D = $Shield/ShieldSprite
+
+@export var shield_hit_sounds: Array[AudioStreamMP3]
+
 var stunned: bool = false
 
 
@@ -12,6 +15,7 @@ func _physics_process(_delta):
 func _on_shield_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player_bullets"):
 		area.queue_free()
+		AudioStreamManager.play(shield_hit_sounds[(randi() % len(shield_hit_sounds))])
 		stun()
 	
 func stun() -> void:
