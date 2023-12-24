bea6914140ea84dade68f5f6ba80115ab151261d
Mark Feineigle <mfeineigle@gmail.com>
2023-12-24 15:57:45 -0500

    add trap_cave_in

---
+++ set_pieces/trap_cave_in.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+## Holds the boulders to cave in
+@export var boulders: Node2D
+## Do the boulders start invisible?
+@export var invisible_boulders: bool = false
+@export var animation_player: AnimationPlayer
+var triggered: bool = false
+
+
+func _ready() -> void:
+	for boulder in boulders.get_children():
+		if invisible_boulders:
+			boulder.visible = false
+		boulder.process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and not triggered:
+		triggered = true
+		animation_player.play("cave_in")
