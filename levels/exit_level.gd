89e81ee296ef5cb65b78ae7ffb42d594a9ec041f
Mark Feineigle <mfeineigle@gmail.com>
2023-12-26 12:36:43 -0500

    move flag_pole animation to flag_pole.gd

--- levels/exit_level.gd
+++ levels/exit_level.gd
@@ -6,9 +6,6 @@ extends Area2D
 
 
 func _on_exit_level_area_body_entered(body: Node2D) -> void:
-	flag_pole.animation_player.play("lower_Iraq_flag")
-	await flag_pole.animation_player.animation_finished
-	flag_pole.animation_player.play("raise_USA_flag")
-	await flag_pole.animation_player.animation_finished
+	await flag_pole.capture_flag()
 	if body.is_in_group("player"):
 		GameEvents.level_changed.emit(next_level)
--- set_pieces/flag_pole.gd
+++ set_pieces/flag_pole.gd
@@ -1,3 +1,9 @@
 extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func capture_flag() -> void:
+	animation_player.play("lower_Iraq_flag")
+	await animation_player.animation_finished
+	animation_player.play("raise_USA_flag")
+	await animation_player.animation_finished
