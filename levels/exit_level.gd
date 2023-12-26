c8b9c20767a26ec25e55145f67b7b5518250554e
Mark Feineigle <mfeineigle@gmail.com>
2023-12-26 12:30:57 -0500

    add flag_pole and animation to exit_level

--- levels/exit_level.gd
+++ levels/exit_level.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
+@onready var flag_pole: Area2D = $FlagPole
+
 @export var next_level: String = ""
 
 
 func _on_exit_level_area_body_entered(body: Node2D) -> void:
+	flag_pole.animation_player.play("lower_Iraq_flag")
+	await flag_pole.animation_player.animation_finished
+	flag_pole.animation_player.play("raise_USA_flag")
+	await flag_pole.animation_player.animation_finished
 	if body.is_in_group("player"):
 		GameEvents.level_changed.emit(next_level)
