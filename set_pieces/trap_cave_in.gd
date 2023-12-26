0c36170b8fa666b9276a8c31c655d6b335ef818a
Mark Feineigle <mfeineigle@gmail.com>
2023-12-26 10:53:28 -0500

    move AudioStreamPlayer to base Node, @export mp3

--- set_pieces/trap_cave_in.gd
+++ set_pieces/trap_cave_in.gd
@@ -1,15 +1,20 @@
 extends Area2D
 
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
 ## Holds the boulders to cave in
 @export var boulders: Node2D
 ## Do the boulders start invisible?
 @export var invisible_boulders: bool = false
-@export var audio_player: AudioStreamPlayer
+## Sound to play when trap triggers
+@export var cave_in_sound_effect: AudioStream
+## Unique animation per trap, add per instance
 @export var animation_player: AnimationPlayer
 var triggered: bool = false
 
 
 func _ready() -> void:
+	audio_stream_player.stream = cave_in_sound_effect
 	for boulder in boulders.get_children():
 		if invisible_boulders:
 			boulder.visible = false
@@ -20,4 +25,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and not triggered:
 		triggered = true
 		animation_player.play("cave_in")
-		audio_player.play()
+		audio_stream_player.play()
