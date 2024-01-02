d1bb46a13692d10a53bff5a46c2549b72f0d5ce4
Mark Feineigle <mfeineigle@gmail.com>
2024-01-02 04:46:15 -0500

    move flagpole to @export
    - allows independent positioning

--- levels/exit_level.gd
+++ levels/exit_level.gd
@@ -1,11 +1,10 @@
 extends Area2D
 
-@onready var flag_pole: Area2D = $FlagPole
-
+@export var flag_pole: Area2D
 @export var next_level: String = ""
 
 
 func _on_exit_level_area_body_entered(body: Node2D) -> void:
-	await flag_pole.capture_flag()
 	if body.is_in_group("player"):
+		await flag_pole.capture_flag()
 		GameEvents.level_exited.emit(next_level)
