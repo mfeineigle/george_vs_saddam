7bbe276c6b47021c05932cb1e6d2c6d45ba546bc
Mark Feineigle <mfeineigle@gmail.com>
2024-01-05 03:57:55 -0500

    refactor base building.tscn and building.gd
    - create building_tileset.tres to paint buildings
      - physics for the wall color

--- set_pieces/buildings/building.gd
+++ set_pieces/buildings/building.gd
@@ -4,10 +4,11 @@ extends StaticBody2D
 @onready var door_animation_player: AnimationPlayer = $DoorAnimationPlayer
 @onready var locked_door_rattle: AudioStreamPlayer = $LockedDoorRattle
 @onready var door_opening: AudioStreamPlayer = $DoorOpening
+@onready var door: Sprite2D = $Door
 
 ## A Node2D that must be added to hold the characters in the building
 @export var occupants: Node2D
-@export var door_locked: bool = false
+@export var is_door_locked: bool = false
 @export var lock_color: Keycard
 
 var door_open: bool = false
@@ -18,12 +19,12 @@ func _ready() -> void:
 
 
 func _on_open_door_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and door_locked:
+	if body.is_in_group("player") and is_door_locked:
 		if lock_color in KeyManager.keys:
-			door_locked = false
+			is_door_locked = false
 		else:
 			locked_door_rattle.play()
-	if body.is_in_group("player") and not door_locked:
+	if body.is_in_group("player") and not is_door_locked:
 		roof_animation_player.play("fade_out")
 		show_occupants()
 		if not door_open:
@@ -33,7 +34,7 @@ func _on_open_door_area_body_entered(body: Node2D) -> void:
 
 
 func _on_open_door_area_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player") and not door_locked:
+	if body.is_in_group("player") and not is_door_locked:
 		roof_animation_player.play_backwards("fade_out")
 		hide_occupants()
 
@@ -42,7 +43,6 @@ func show_occupants() -> void:
 	if occupants:
 		for i in occupants.get_children():
 			i.visible = true
-
 
 func hide_occupants() -> void:
 	if occupants:
