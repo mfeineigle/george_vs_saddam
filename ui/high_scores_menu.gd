9454e64d42379c824e23b344b65c708abb3fd439
Mark Feineigle <mfeineigle@gmail.com>
2024-01-11 13:26:06 -0500

    add stars to high score menu

--- ui/high_scores_menu.gd
+++ ui/high_scores_menu.gd
@@ -2,21 +2,45 @@ extends Control
 
 @onready var level_option_button: OptionButton = $VBoxContainer/LevelOptionButton
 
-@onready var best_time_label_01: Label = $VBoxContainer/TimeHBox1/BestTimeLabel01
-@onready var best_time_label_02: Label = $VBoxContainer/TimeHBox2/BestTimeLabel02
-@onready var best_time_label_03: Label = $VBoxContainer/TimeHBox3/BestTimeLabel03
-@onready var best_time_label_04: Label = $VBoxContainer/TimeHBox4/BestTimeLabel04
-@onready var best_time_label_05: Label = $VBoxContainer/TimeHBox5/BestTimeLabel05
+@onready var best_time_label_01: Label = $VBoxContainer/FirstHBox/TimeHBox1/BestTimeLabel01
+@onready var best_time_label_02: Label = $VBoxContainer/SecondHBox/TimeHBox2/BestTimeLabel02
+@onready var best_time_label_03: Label = $VBoxContainer/ThirdHbox/TimeHBox3/BestTimeLabel03
+@onready var best_time_label_04: Label = $VBoxContainer/FourthHbox/TimeHBox4/BestTimeLabel04
+@onready var best_time_label_05: Label = $VBoxContainer/FifthHbox/TimeHBox5/BestTimeLabel05
+
+@onready var stars_1: HBoxContainer = $VBoxContainer/FirstHBox/Stars1
+@onready var stars_2: HBoxContainer = $VBoxContainer/SecondHBox/Stars2
+@onready var stars_3: HBoxContainer = $VBoxContainer/ThirdHbox/Stars3
+@onready var stars_4: HBoxContainer = $VBoxContainer/FourthHbox/Stars4
+@onready var stars_5: HBoxContainer = $VBoxContainer/FifthHbox/Stars5
+@onready var all_stars: Array = [stars_1, stars_2, stars_3, stars_4, stars_5]
 
 
 func _ready() -> void:
 	level_option_button.add_item("A")
 	level_option_button.add_item("B")
 	level_option_button.add_item("C")
-	var selected_level: String = level_option_button.get_item_text(0)
-	var times = Utils.read_best_times(selected_level)
+	_on_level_option_button_item_selected(0)
+
+
+func _on_level_option_button_item_selected(index: int) -> void:
+	var selected_level: String = level_option_button.get_item_text(index).to_lower()
+	var times: Array = Utils.read_best_times(selected_level)
+	var level = load("res://levels/"+"level_test_"+selected_level+".tscn").instantiate()
+	var time_goals: Dictionary = level.get_meta("TimeGoals")
+	update_stars(times, time_goals)
 	update_high_scores(times)
 
+func update_stars(times: Array, time_goals: Dictionary) -> void:
+	for i in range(times.size()):
+		for star in all_stars[i].get_children():
+			star.hide()
+		if times[i] < time_goals[1]:
+			all_stars[i].get_child(0).show()
+		if times[i] < time_goals[2]:
+			all_stars[i].get_child(1).show()
+		if times[i] < time_goals[3]:
+			all_stars[i].get_child(2).show()
 
 func update_high_scores(times) -> void:
 	best_time_label_01.text = convert_time_to_displayable(times[0])
@@ -25,18 +49,11 @@ func update_high_scores(times) -> void:
 	best_time_label_04.text = convert_time_to_displayable(times[3])
 	best_time_label_05.text = convert_time_to_displayable(times[4])
 
-
 func convert_time_to_displayable(time) -> String:
 	var mils = int(fmod(time, 1)*1000)
 	var secs = int(fmod(time, 60))
 	var mins = int(fmod(time, 60*60)/60)
 	return str("%02d:%02d.%03d" % [mins, secs, mils])
-
-
-func _on_level_option_button_item_selected(index: int) -> void:
-	var selected_level: String = level_option_button.get_item_text(index).to_lower()
-	var times: Array = Utils.read_best_times(selected_level)
-	update_high_scores(times)
 
 
 func _on_reset_button_pressed() -> void:
