8c7ae31d04f151585c652706958fa6ca77d990f0
Mark Feineigle <mfeineigle@gmail.com>
2024-01-15 14:56:24 -0500

    add secret found label and sound effect

--- collectibles/secret_areas/secret_area.gd
+++ collectibles/secret_areas/secret_area.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var secret_found: AudioStreamPlayer = $SecretFound
+
 
 var is_found: bool = false
 @export var id: String
@@ -8,5 +10,5 @@ var is_found: bool = false
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and not is_found:
 		is_found = true
-		print("boom")
+		secret_found.play()
 		GameEvents.secret_found.emit(id)
---
+++ ui/SecretFoundLabel.gd
@@ -0,0 +1,16 @@
+extends Label
+
+
+func _ready() -> void:
+	hide()
+	GameEvents.connect("secret_found", _on_secret_found)
+
+
+func _on_secret_found(_id) -> void:
+	self.modulate.a = 1
+	show()
+	await get_tree().create_timer(2).timeout
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "modulate:a", 0, 1.0)
+	await tween.finished
+	hide()
