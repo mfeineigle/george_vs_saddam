2d28162b5da776f41f5925a8891c1c5f296839f0
Mark Feineigle <mfeineigle@gmail.com>
2024-01-16 15:40:54 -0500

    add variance to flag waving shader

--- set_pieces/flag_pole.gd
+++ set_pieces/flag_pole.gd
@@ -1,6 +1,19 @@
 extends Area2D
 
+@onready var flag_usa: Polygon2D = $flag_USA
+@onready var flag_iraq: Polygon2D = $flag_Iraq
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	# add some variance to the shader animation
+	var speed_offset: float = randf_range(-0.05, 0.05)
+	flag_usa.material.set_shader_parameter("speed", 2 + speed_offset)
+	flag_iraq.material.set_shader_parameter("speed", 2 + speed_offset)
+	var freq_y_offset: float = randf_range(-0.05, 0.05)
+	flag_usa.material.set_shader_parameter("frequency_y", 2 + freq_y_offset)
+	flag_iraq.material.set_shader_parameter("frequency_y", 2 + freq_y_offset)
+
 
 func capture_flag() -> void:
 	animation_player.play("lower_Iraq_flag")
