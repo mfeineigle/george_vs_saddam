58a854af4acf6ac24cca1fc2df5495926a9acf75
Mark Feineigle <mfeineigle@gmail.com>
2024-01-29 19:42:49 -0500

    add sniper

---
+++ enemies/sniper.gd
@@ -0,0 +1,34 @@
+extends Soldier
+
+var can_run_away: bool = false
+
+
+func _ready() -> void:
+	weapon = $Weapons/Rifle
+	super._ready()
+
+
+func _physics_process(_delta: float) -> void:
+	if get_new_nav and not $NavigationAgent2D.is_navigation_finished():
+		velocity = update_nav()
+	direction = (Globals.player_pos - position).normalized()
+	shoot()
+	if can_run_away and not is_sentinel:
+		look_away(Globals.player_pos)
+		direction = (Globals.player_pos - position).normalized()
+		velocity = speed * -direction
+		move_and_slide()
+	set_spirte_direction()
+
+
+func look_away(target_pos):
+	rotate(get_angle_to(target_pos) + PI);
+
+
+func _on_run_away_area_inner_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		can_run_away = true
+
+func _on_run_away_area_outer_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		can_run_away = false
