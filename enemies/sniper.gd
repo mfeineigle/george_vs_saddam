b2da0174db9f3d9ec6f96b9c01d32700c141c2dc
Mark Feineigle <mfeineigle@gmail.com>
2024-01-29 20:04:04 -0500

    use sniper_bullet, only shoot when not moving

--- enemies/sniper.gd
+++ enemies/sniper.gd
@@ -1,10 +1,13 @@
 extends Soldier
 
-var can_run_away: bool = false
+var is_running_away: bool = false
 
 
 func _ready() -> void:
 	weapon = $Weapons/Rifle
+	print(weapon.bullet_scene)
+	weapon.bullet_scene = preload("res://projectiles/sniper_bullet.tscn")
+	print(weapon.bullet_scene)
 	super._ready()
 
 
@@ -12,8 +15,9 @@ func _physics_process(_delta: float) -> void:
 	if get_new_nav and not $NavigationAgent2D.is_navigation_finished():
 		velocity = update_nav()
 	direction = (Globals.player_pos - position).normalized()
-	shoot()
-	if can_run_away and not is_sentinel:
+	if not is_running_away:
+		shoot()
+	if is_running_away and not is_sentinel:
 		look_away(Globals.player_pos)
 		direction = (Globals.player_pos - position).normalized()
 		velocity = speed * -direction
@@ -27,8 +31,8 @@ func look_away(target_pos):
 
 func _on_run_away_area_inner_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		can_run_away = true
+		is_running_away = true
 
 func _on_run_away_area_outer_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		can_run_away = false
+		is_running_away = false
