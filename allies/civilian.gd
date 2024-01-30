27ae910514365a0bb4198b02fe4d0957641b34c4
Mark Feineigle <mfeineigle@gmail.com>
2024-01-29 20:17:11 -0500

    update run_away hysteresis"
    - remove timer
    - add 2 collison shapes (inner/outer) to toggle run_away

--- allies/civilian.gd
+++ allies/civilian.gd
@@ -5,6 +5,7 @@ class_name Civilian extends CharacterBody2D
 
 @export var speed: int = 250
 @export var hit_sounds: Array[AudioStreamMP3]
+
 var blood_scene: PackedScene = preload("res://enemies/blood.tscn")
 var run_away: bool = false
 var direction: Vector2
@@ -13,8 +14,8 @@ var is_dead: bool = false
 
 func _ready() -> void:
 	sprite.show()
-	
-	
+
+
 func _physics_process(_delta) -> void:
 	look_at(Globals.player_pos)
 	if rotation >= PI or rotation <= -PI:
@@ -27,17 +28,6 @@ func _physics_process(_delta) -> void:
 	Utils.rotate_sprite_direction(sprite, rotation)
 
 
-func _on_avoidance_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		run_away = true
-
-func _on_avoidance_area_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player") and not is_dead:
-		$Timers/KeepRunningTimer.start()
-
-func _on_keep_running_timer_timeout() -> void:
-	run_away = false
-	
 func look_away(target_pos):
 	rotate(get_angle_to(target_pos) + PI);
 
@@ -57,3 +47,12 @@ func die() -> void:
 	#get_owner().get_node("Background/Blood").add_child(blood)
 	blood.setup(global_position)
 	queue_free()
+
+
+func _on_run_away_area_inner_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		run_away = true
+
+func _on_run_away_area_outer_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		run_away = false
