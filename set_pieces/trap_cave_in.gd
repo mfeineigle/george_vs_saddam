1333b17abffa6dc024956a6a752e2c067f5697e0
Mark Feineigle <mfeineigle@gmail.com>
2024-02-14 12:19:44 -0500

    add navRegions to trap_cave_in's boulders
    - the navRegions go over "holes" in the level's navRegion
    - starting boulders navRegion is disabled to start (no nav)
    - finished boulders navRegion is enabled to start (yes nav)
    - on trigger the navRegions toggle en/disabled

--- set_pieces/trap_cave_in.gd
+++ set_pieces/trap_cave_in.gd
@@ -10,6 +10,10 @@ extends Area2D
 @export var cave_in_sound_effect: AudioStream
 ## Unique animation per trap, add per instance
 @export var animation_player: AnimationPlayer
+## Nav region to block/allow after the trap triggers
+@export var nav_region_starting_rocks: NavigationRegion2D
+@export var nav_region_finished_rocks: NavigationRegion2D
+
 var triggered: bool = false
 
 
@@ -19,13 +23,25 @@ func _ready() -> void:
 		if invisible_boulders:
 			boulder.visible = false
 		boulder.process_mode = Node.PROCESS_MODE_DISABLED
+	if nav_region_starting_rocks:
+		nav_region_starting_rocks.enabled = false
+		nav_region_starting_rocks.use_edge_connections = false
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and not triggered:
 		triggered = true
+		_update_nav_regions()
 		Globals.trap_triggers += 1
 		animation_player.play("cave_in")
 		audio_stream_player.play()
 		for boulder in boulders.get_children():
 			boulder.process_mode = Node.PROCESS_MODE_INHERIT
+
+func _update_nav_regions() -> void:
+	if nav_region_starting_rocks:
+		nav_region_starting_rocks.enabled = true
+		nav_region_starting_rocks.use_edge_connections = true
+	if nav_region_starting_rocks:
+		nav_region_finished_rocks.enabled = false
+		nav_region_finished_rocks.use_edge_connections = false
