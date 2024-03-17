622abc8d6b8bbdd3b057a4d69c2e4fcbcf4210a2
Mark Feineigle <mfeineigle@gmail.com>
2024-03-17 14:47:29 -0400

    add flag capture sfx

--- set_pieces/flags/flag_point.gd
+++ set_pieces/flags/flag_point.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
 @onready var flag_pole: Area2D = $FlagPole
+@onready var collect_sfx: AudioStreamPlayer = $AudioStreamPlayer
 
 var scored: bool = false
+
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and not scored:
 		scored = true
 		Globals.flag_captures += 1
+		collect_sfx.play()
 		flag_pole.capture_flag()
