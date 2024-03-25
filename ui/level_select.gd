2bdc982804f4d8acabe9bd5b562dc587543ea397
Mark Feineigle <mfeineigle@gmail.com>
2024-03-24 20:35:37 -0400

    use unique IDs, add 'esc' goto start_menu

--- ui/level_select.gd
+++ ui/level_select.gd
@@ -4,6 +4,15 @@ extends Node2D
 var save_path = "user://data/levels/last_level.save"
 var last_level: String
 
+@onready var lvl_01_button: Button = %Lvl_01_Button
+@onready var lvl_a_button: Button = %Lvl_A_Button
+@onready var lvl_b_button: Button = %Lvl_B_Button
+@onready var lvl_c_button: Button = %Lvl_C_Button
+@onready var lvl_d_button: Button = %Lvl_D_Button
+@onready var lvl_z_button: Button = %Lvl_Z_Button
+@onready var start_menu_button: Button = %StartMenu_Button
+@onready var quit_button: Button = %Quit_Button
+
 
 func _ready() -> void:
 	last_level = "1"
@@ -14,15 +23,22 @@ func _ready() -> void:
 			print(last_level)
 	match last_level:
 		"res://levels/level_01.tscn":
-			$CanvasGroup/VBoxContainer/Lvl_01_Button.grab_focus()
+			lvl_01_button.grab_focus()
 		"res://levels/level_test_a.tscn":
-			$CanvasGroup/VBoxContainer/Lvl_A_Button.grab_focus()
+			lvl_a_button.grab_focus()
 		"res://levels/level_test_b.tscn":
-			$CanvasGroup/VBoxContainer/Lvl_B_Button.grab_focus()
+			lvl_b_button.grab_focus()
 		"res://levels/level_test_c.tscn":
-			$CanvasGroup/VBoxContainer/Lvl_C_Button.grab_focus()
+			lvl_c_button.grab_focus()
 		"res://levels/level_test_d.tscn":
-			$CanvasGroup/VBoxContainer/Lvl_D_Button.grab_focus()
+			lvl_d_button.grab_focus()
+		"res://levels/level_test_z.tscn":
+			lvl_z_button.grab_focus()
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		_on_start_menu_button_pressed()
 
 
 func _on_lvl_01_button_pressed() -> void:
@@ -59,6 +75,10 @@ func _on_lvl_z_button_pressed() -> void:
 	var file = FileAccess.open(save_path, FileAccess.WRITE)
 	file.store_var("user://levels/level_test_z.tscn")
 	GameEvents.level_changed.emit("res://levels/level_test_z.tscn")
+
+
+func _on_start_menu_button_pressed() -> void:
+	GameEvents.level_changed.emit("res://ui/start_menu.tscn")
 
 
 func _on_quit_button_pressed() -> void:
